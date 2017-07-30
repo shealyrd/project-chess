39cc767635a1a71d98568fe540589aa12d1570e9
shealyrd <shealyrd@email.uscupstate.edu>
2017-07-30 00:13:13 -0400

    initial-commit

---
+++ src/Pos.java
@@ -0,0 +1,25 @@
+
+public class Pos {
+	private int X;
+	private int Y;
+	
+	public Pos(int x, int y) {
+		X = x;
+		Y = y;
+	}
+	
+	public int getX() {
+		return X;
+	}
+	public void setX(int x) {
+		X = x;
+	}
+	public int getY() {
+		return Y;
+	}
+	public void setY(int y) {
+		Y = y;
+	}
+	
+	
+}
